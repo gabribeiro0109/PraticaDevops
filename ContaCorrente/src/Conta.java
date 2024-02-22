2f1c2da4577fcf1134c9a6ce5f164a0a38ec5113
gabribeiro0109 <124214510+gabribeiro0109@users.noreply.github.com>
2024-02-22 20:01:21 -0300

    Subindo Projeto

---
+++ ContaCorrente/src/Conta.java
@@ -0,0 +1,30 @@
+
+public class Conta {
+    private String nome;
+    private double saldo;
+    private double retirarsaldo;
+
+
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }//Fecha método setNome
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    public void setRetirarsaldoaldo(double retirarsaldo) {
+        this.retirarsaldo = retirarsaldo;
+    }
+    public String getNome(){
+      return nome;
+    }//Fecha método getNome
+
+
+    public double getSaldo(){
+        return saldo;
+    }
+
+    public double getRetirarsaldo(){
+        return retirarsaldo;
+    }
+}//Fecha classe
